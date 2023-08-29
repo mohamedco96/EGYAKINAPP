781813811145664be81a4738ec9632729f361e54
Mohamed Ibrahim <32494528+mohamedco96@users.noreply.github.com>
2023-08-29 14:24:05 +0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::apiresource('products',ProductController::class);
+//Route::apiresource('products',ProductController::class);
 
 //Public routes
 //Route::get('/products/search/{name}',[ProductController::class,'search']);

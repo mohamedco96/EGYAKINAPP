6716fbc92be5c3358b907e2f21f229f6d6beaa31
Mohamed Ibrahim <32494528+mohamedco96@users.noreply.github.com>
2023-10-04 16:44:47 +0300

    SectionSeeder

--- database/factories/SectionFactory.php
+++ database/factories/SectionFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Section>
@@ -17,7 +18,17 @@ class SectionFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'owner_id' => fake()->numberBetween(1,50),
+            'patient_id' => fake()->numberBetween(1,200),
+            'section_1' => fake()->boolean(100),
+            'section_2' => fake()->boolean(0),
+            'section_3' => fake()->boolean(0),
+            'section_4' => fake()->boolean(0),
+            'section_5' => fake()->boolean(0),
+            'section_6' => fake()->boolean(0),
+            'section_7' => fake()->boolean(0),
+            'submit_status' => fake()->boolean(0),
+            'outcome_status' => fake()->boolean(0),
         ];
     }
 }

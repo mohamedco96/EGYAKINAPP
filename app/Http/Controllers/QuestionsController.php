cc81b480fd07cc90465ed035e7939a03a6fece46
Mohamed Ibrahim <32494528+mohamedco96@users.noreply.github.com>
2023-10-28 02:22:15 +0200

    Update QuestionsController.php

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -272,7 +272,302 @@ class QuestionsController extends Controller
                     
                     $data[] = $question;
                 }
-                break;               
+                break;    
+                case 4:
+                    $data = [];
+                    for ($i = 33; $i <= 46; $i++) {
+                        $questions = Questions::where('section_id', $section_id)
+                        ->where('id', $i)
+                        ->select('id', 'question', 'values', 'type', 'keyboard_type','mandatory', 'updated_at')
+                        ->first();
+            
+                        $answers = Risk::where('patient_id', $patient_id)
+                        ->select(
+                            'id','CKD_history','AK_history','cardiac-failure_history','LCF_history','neurological-impairment_disability_history',
+                            'sepsis_history','contrast_media','drugs-with-potential-nephrotoxicity','drug_name','hypovolemia_history',
+                            'malignancy_history','trauma_history','autoimmune-disease_history','other-risk-factors','other'
+                        )
+                        ->first();
+            
+                        $question = [
+                            'id' => $questions->{'id'},
+                            'question' => $questions->{'question'},
+                            'values' => $questions->{'values'},
+                            'type' => $questions->{'type'},
+                            'keyboard_type' => $questions->{'keyboard_type'},
+                            'mandatory' => $questions->{'mandatory'},
+                            'updated_at' => $questions->{'updated_at'},
+                        ];
+            
+                        switch ($i) {
+                            case 33:
+                                $question['answer'] = $answers->{'CKD_history'};
+                                break;
+                            case 34:
+                                $question['answer'] = $answers->{'AK_history'};
+                                break;
+                            case 35:
+                                $question['answer'] = $answers->{'cardiac-failure_history'};
+                                break;
+                            case 36:
+                                $question['answer'] = $answers->{'LCF_history'};
+                                break;
+                            case 37:
+                                $question['answer'] = $answers->{'neurological-impairment_disability_history'};
+                                break;
+                            case 38:
+                                $question['answer'] = $answers->{'sepsis_history'};
+                                break;
+                            case 39:
+                                $question['answer'] = $answers->{'contrast_media'};
+                                break;
+                            case 40:
+                                $question['answer'] = $answers->{'drugs-with-potential-nephrotoxicity'};
+                            break;
+                            case 41:
+                                $question['answer'] = $answers->{'drug_name'};
+                                break;
+                            case 42:
+                                $question['answer'] = $answers->{'hypovolemia_history'};
+                                break;
+                            case 43:
+                                $question['answer'] = $answers->{'malignancy_history'};
+                                break;
+                            case 44:
+                                $question['answer'] = $answers->{'trauma_history'};
+                                break;
+                            case 45:
+                                $question['answer'] = $answers->{'autoimmune-disease_history'};
+                                break;
+                            case 46:
+                                $question['answer'] = $answers->{'other-risk-factors'};
+                                break;
+                                
+                        }
+                        
+                        $data[] = $question;
+                    }
+                break;
+                case 5:
+                    $data = [];
+                    for ($i = 47; $i <= 69; $i++) {
+                        $questions = Questions::where('section_id', $section_id)
+                        ->where('id', $i)
+                        ->select('id', 'question', 'values', 'type', 'keyboard_type','mandatory', 'updated_at')
+                        ->first();
+            
+                        $answers = Assessment::where('patient_id', $patient_id)
+                        ->select(
+                            'id','heart-rate/minute','respiratory-rate/minute','SBP','DBP','GCS','oxygen_saturation','temperature','UOP','AVPU',
+                            'skin_examination','skin_examination_clarify','eye_examination','eye_examination_clarify','ear_examination',
+                            'ear_examination_clarify','cardiac_examination','cardiac_examination_clarify','internal_jugular_vein','chest_examination',
+                            'chest_examination_clarify','abdominal_examination','abdominal_examination_clarify'
+                        )
+                        ->first();
+            
+                        $question = [
+                            'id' => $questions->{'id'},
+                            'question' => $questions->{'question'},
+                            'values' => $questions->{'values'},
+                            'type' => $questions->{'type'},
+                            'keyboard_type' => $questions->{'keyboard_type'},
+                            'mandatory' => $questions->{'mandatory'},
+                            'updated_at' => $questions->{'updated_at'},
+                        ];
+            
+                        switch ($i) {
+                            case 47:
+                                $question['answer'] = $answers->{'heart-rate/minute'};
+                                break;
+                            case 48:
+                                $question['answer'] = $answers->{'respiratory-rate/minute'};
+                                break;
+                            case 49:
+                                $question['answer'] = $answers->{'SBP'};
+                                break;
+                            case 50:
+                                $question['answer'] = $answers->{'DBP'};
+                                break;
+                            case 51:
+                                $question['answer'] = $answers->{'GCS'};
+                                break;
+                            case 52:
+                                $question['answer'] = $answers->{'oxygen_saturation'};
+                                break;
+                            case 53:
+                                $question['answer'] = $answers->{'temperature'};
+                                break;
+                            case 54:
+                                $question['answer'] = $answers->{'UOP'};
+                            break;
+                            case 55:
+                                $question['answer'] = $answers->{'AVPU'};
+                                break;
+                            case 56:
+                                $question['answer'] = $answers->{'skin_examination'};
+                                break;
+                            case 57:
+                                $question['answer'] = $answers->{'skin_examination_clarify'};
+                                break;
+                            case 58:
+                                $question['answer'] = $answers->{'eye_examination'};
+                                break;
+                            case 59:
+                                $question['answer'] = $answers->{'eye_examination_clarify'};
+                                break;
+                            case 60:
+                                $question['answer'] = $answers->{'ear_examination'};
+                                break;
+                            case 61:
+                                $question['answer'] = $answers->{'ear_examination_clarify'};
+                                break;
+                            case 62:
+                                $question['answer'] = $answers->{'cardiac_examination'};
+                                break;
+                            case 63:
+                                $question['answer'] = $answers->{'cardiac_examination_clarify'};
+                                break;
+                            case 64:
+                                $question['answer'] = $answers->{'internal_jugular_vein'};
+                                break;
+                            case 65:
+                                $question['answer'] = $answers->{'chest_examination'};
+                                break;
+                            case 66:
+                                $question['answer'] = $answers->{'chest_examination_clarify'};
+                                break;
+                            case 67:
+                                $question['answer'] = $answers->{'abdominal_examination'};
+                                break;
+                            case 68:
+                                $question['answer'] = $answers->{'abdominal_examination_clarify'};
+                                break;
+                            case 69:
+                                $question['answer'] = $answers->{'other'};
+                                break;
+                                
+                        }
+                        
+                        $data[] = $question;
+                    }
+                break;
+                case 6:
+                    $data = [];
+                    for ($i = 70; $i <= 73; $i++) {
+                        $questions = Questions::where('section_id', $section_id)
+                        ->where('id', $i)
+                        ->select('id', 'question', 'values', 'type', 'keyboard_type','mandatory', 'updated_at')
+                        ->first();
+            
+                        $answers = Examination::where('patient_id', $patient_id)
+                        ->select(
+                            'id','current_creatinine','basal_creatinine','renal_US','specify_renal-US'
+                        )
+                        ->first();
+            
+                        $question = [
+                            'id' => $questions->{'id'},
+                            'question' => $questions->{'question'},
+                            'values' => $questions->{'values'},
+                            'type' => $questions->{'type'},
+                            'keyboard_type' => $questions->{'keyboard_type'},
+                            'mandatory' => $questions->{'mandatory'},
+                            'updated_at' => $questions->{'updated_at'},
+                        ];
+            
+                        switch ($i) {
+                            case 70:
+                                $question['answer'] = $answers->{'current_creatinine'};
+                                break;
+                            case 71:
+                                $question['answer'] = $answers->{'basal_creatinine'};
+                                break;
+                            case 72:
+                                $question['answer'] = $answers->{'renal_US'};
+                                break;
+                            case 73:
+                                $question['answer'] = $answers->{'specify_renal-US'};
+                                break;
+                        }
+                        
+                        $data[] = $question;
+                    }
+                break;
+                case 7:
+                    $data = [];
+                    for ($i = 74; $i <= 74; $i++) {
+                        $questions = Questions::where('section_id', $section_id)
+                        ->where('id', $i)
+                        ->select('id', 'question', 'values', 'type', 'keyboard_type','mandatory', 'updated_at')
+                        ->first();
+            
+                        $answers = Decision::where('patient_id', $patient_id)
+                        ->select(
+                            'id','medical_decision'
+                        )
+                        ->first();
+            
+                        $question = [
+                            'id' => $questions->{'id'},
+                            'question' => $questions->{'question'},
+                            'values' => $questions->{'values'},
+                            'type' => $questions->{'type'},
+                            'keyboard_type' => $questions->{'keyboard_type'},
+                            'mandatory' => $questions->{'mandatory'},
+                            'updated_at' => $questions->{'updated_at'},
+                        ];
+            
+                        switch ($i) {
+                            case 74:
+                                $question['answer'] = $answers->{'medical_decision'};
+                                break;
+                        }
+                        
+                        $data[] = $question;
+                    }
+                break;
+                case 8:
+                    $data = [];
+                    for ($i = 75; $i <= 78; $i++) {
+                        $questions = Questions::where('section_id', $section_id)
+                        ->where('id', $i)
+                        ->select('id', 'question', 'values', 'type', 'keyboard_type','mandatory', 'updated_at')
+                        ->first();
+            
+                        $answers = Outcome::where('patient_id', $patient_id)
+                        ->select(
+                            'id','outcome_of_the_patient','creatinine_on_discharge','final_status','other'
+                        )
+                        ->first();
+            
+                        $question = [
+                            'id' => $questions->{'id'},
+                            'question' => $questions->{'question'},
+                            'values' => $questions->{'values'},
+                            'type' => $questions->{'type'},
+                            'keyboard_type' => $questions->{'keyboard_type'},
+                            'mandatory' => $questions->{'mandatory'},
+                            'updated_at' => $questions->{'updated_at'},
+                        ];
+            
+                        switch ($i) {
+                            case 75:
+                                $question['answer'] = $answers->{'outcome_of_the_patient'};
+                                break;
+                            case 76:
+                                $question['answer'] = $answers->{'creatinine_on_discharge'};
+                                break;
+                            case 77:
+                                $question['answer'] = $answers->{'final_status'};
+                                break;
+                            case 78:
+                                $question['answer'] = $answers->{'other'};
+                                break;
+                        }
+                        
+                        $data[] = $question;
+                    }
+                break;
             }
         $response = [
             'value' => true,

0c5602c8c2f6af15f152d5ac78953c076727ab9e
Mohamed Ibrahim <32494528+mohamedco96@users.noreply.github.com>
2023-12-31 18:40:30 +0200

    Privacy Policy

---
+++ resources/views/policy.blade.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Privacy Policy</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 20px;
+        }
+
+        h1 {
+            color: #333;
+        }
+
+        h2 {
+            color: #555;
+        }
+
+        p {
+            line-height: 1.5;
+            margin-bottom: 10px;
+        }
+
+        ul {
+            margin: 0;
+            padding: 0 0 0 20px;
+            list-style-type: disc;
+        }
+    </style>
+</head>
+<body>
+    <h1>Privacy Policy</h1>
+
+    <p>Last updated: [Date]</p>
+
+    <p>This Privacy Policy governs the manner in which EGYAKIN we collects, uses, maintains, and discloses information collected from users of our website https://egyakin.com/. This Privacy Policy applies to the Site and all products and services offered by EGYAKIN.</p>
+
+    <h2>1. Information we collect</h2>
+
+    <p>We may collect personal identification information from users in various ways, including but not limited to when users visit our site, register on the site, fill out a form, subscribe to our newsletter, or in connection with other activities, services, features, or resources we make available on our Site. Users may be asked for, as appropriate, name, email address, mailing address, phone number, and other relevant information.</p>
+
+    <h2>2. How we use collected information</h2>
+
+    <p>We may collect and use users' personal information for the following purposes:</p>
+
+    <ul>
+        <li>To improve customer service: Information you provide helps us respond to your customer service requests and support needs more efficiently.</li>
+        <li>To personalize user experience: We may use information in the aggregate to understand how our users as a group use the services and resources provided on our Site.</li>
+        <li>To send periodic emails: We may use the email address to send user information and updates pertaining to their order. It may also be used to respond to their inquiries, questions, and/or other requests.</li>
+    </ul>
+
+    <h2>3. How we protect your information</h2>
+
+    <p>We adopt appropriate data collection, storage, and processing practices and security measures to protect against unauthorized access, alteration, disclosure, or destruction of your personal information, username, password, transaction information, and data stored on our Site.</p>
+
+    <h2>4. Sharing your personal information</h2>
+
+    <p>We do not sell, trade, or rent users' personal identification information to others. We may share generic aggregated demographic information not linked to any personal identification information regarding visitors and users with our business partners, trusted affiliates, and advertisers for the purposes outlined above.</p>
+
+    <h2>5. Compliance with legal requirements</h2>
+
+    <p>We will disclose your personal information if required by law or in good faith belief that such action is necessary to comply with legal obligations, protect and defend our rights or property, act in urgent circumstances to protect the personal safety of users of the Site, or protect against legal liability.</p>
+
+    <h2>6. Third-party websites</h2>
+
+    <p>Users may find advertising or other content on our Site that links to the sites and services of our partners, suppliers, advertisers, and sponsors, licensors, and other third parties. We do not control or endorse the content or links that appear on these third-party websites and are not responsible for the practices employed by websites linked to or from our Site. Browsing and interaction on any other website, including websites which have a link to our Site, is subject to that website's own terms and policies.</p>
+
+    <h2>7. Changes to this privacy policy</h2>
+
+    <p>We have the discretion to update this privacy policy at any time. When we do, we will revise the updated date at the bottom of this page. We encourage users to frequently check this page for any changes to stay informed about how we are helping to protect the personal information we collect. You acknowledge and agree that it is your responsibility to review this privacy policy periodically and become aware of modifications.</p>
+
+    <h2>8. Your acceptance of these terms</h2>
+
+    <p>By using this Site, you signify your acceptance of this policy. If you do not agreewith this policy, please do not use ourSite. Your continued use of the Site following the posting of changes to this policy will be deemed your acceptance of those changes.</p>
+
+    <h2>9. Contacting us</h2>
+
+    <p>If you have any questions about this Privacy Policy, the practices of this Site, or your dealings with this Site, please contact us at support@egyakin.com.</p>
+
+    <p>This Privacy Policy applies only to the information collected on our Site and does not apply to any information collected offline or through other channels.</p>
+
+    <p>Please note that this is a general template for a privacy policy and may need to be customized to fit the specific requirements and practices of your website. It is recommended to consult with a legal professional to ensure compliance with applicable privacy laws and regulations.</p>
+</body>
+</html>

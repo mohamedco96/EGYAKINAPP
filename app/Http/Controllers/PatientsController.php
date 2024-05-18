ffa8360e666655062d8bf5015fbf09bff7ec132b
Mohamed Ibrahim <32494528+mohamedco96@users.noreply.github.com>
2024-05-19 02:46:46 +0300

    Update PatientsController.php

--- app/Http/Controllers/PatientsController.php
+++ app/Http/Controllers/PatientsController.php
@@ -2,17 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\PatientHistory;
 use App\Models\Patients;
-use App\Http\Requests\StorePatientsRequest;
 use App\Http\Requests\UpdatePatientsRequest;
-
 use App\Models\PatientStatus;
 use App\Models\Posts;
 use App\Models\Questions;
-use App\Models\Score;
-use App\Models\ScoreHistory;
-use App\Models\SectionsInfo;
 use App\Models\User;
 use App\Models\Answers;
 use App\Models\Notification;
@@ -21,8 +15,6 @@ use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Facades\Storage;
-use PhpOption\None;
 use PDF;
 
 class PatientsController extends Controller
@@ -78,7 +70,7 @@ class PatientsController extends Controller
                         ->whereColumn('users.id', 'scores.doctor_id')
                         ->limit(1);
                 }, 'score')
-                ->orderByRaw('patients_count DESC, COALESCE(score, 0) DESC')
+                ->orderByRaw('COALESCE(score, 0) DESC,patients_count DESC')
                 ->get()
                 ->map(function ($user) {
                     $user->patients_count = strval($user->patients_count);
@@ -296,60 +288,10 @@ class PatientsController extends Controller
         }
     }
 
-
-    public function doctorPatientGetold()
-    {
-        try {
-            $user = Auth::user();
-            $currentPatients = $user->patients()
-                ->select('id', 'doctor_id', 'updated_at')
-                ->where('hidden', false)
-                ->with(['doctor' => function ($query) {
-                    $query->select('id', 'name', 'lname', 'image');
-                }])
-                ->with(['status' => function ($query) {
-                    $query->select('id', 'patient_id', 'key', 'status')
-                        ->where(function ($query) {
-                            $query->where('key', 'LIKE', 'submit_status')
-                                ->orWhere('key', 'LIKE', 'outcome_status');
-                        });
-                }])
-                ->with(['answers' => function ($query) {
-                    $query->select('id', 'patient_id', 'answer')
-                        ->whereIn('question_id', [1, 2]); // Adjusted condition using whereIn
-                }])
-                ->latest('updated_at')
-                ->paginate(10);
-
-            $userPatientCount = $user->patients()->count();
-            $scoreValue = optional($user->score)->score ?? 0;
-            $isVerified = (bool)$user->email_verified_at;
-
-            $response = [
-                'value' => true,
-                'verified' => $isVerified,
-                'patient_count' => strval($userPatientCount),
-                'score_value' => strval($scoreValue),
-                'data' => $currentPatients,
-            ];
-
-            // Log successful response
-            Log::info('Successfully retrieved current patient data for ', ['doctor_id' => optional(Auth::user())->id]);
-
-            return response()->json($response, 200);
-        } catch (\Exception $e) {
-            // Log error
-            Log::error('Error retrieving doctor patient data.', ['doctor_id' => optional(Auth::user())->id, 'exception' => $e]);
-
-            // Return error response
-            return response()->json(['error' => 'Failed to retrieve doctor patient data.'], 500);
-        }
-    }
-
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function storePatient(Request $request)
     {
         try {
             // Start a database transaction
@@ -457,7 +399,7 @@ class PatientsController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdatePatientsRequest $request, $section_id, $patient_id)
+    public function updatePatient(UpdatePatientsRequest $request, $section_id, $patient_id)
     {
         // Check if the section exists
         $sectionExists = Questions::where('section_id', $section_id)->exists();
@@ -655,255 +597,9 @@ class PatientsController extends Controller
     }
 
     /**
-     * Update the specified resource in storage.
-     */
-    public function updateFinalSubmit(UpdatePatientsRequest $request, $patient_id)
-    {
-        $patientSubmitStatus = PatientStatus::where('patient_id', $patient_id)
-            ->where('key', 'submit_status')->first();
-
-        if (!$patientSubmitStatus) {
-            $response = [
-                'value' => false,
-                'message' => 'Patient not found',
-            ];
-
-            return response()->json($response, 404);
-        }
-
-        // Update submit status
-        $patientSubmitStatus->update(['status' => true]);
-
-        // Scoring system
-        $doctorId = Auth::id();
-        $incrementAmount = 4;
-        $action = 'Final Submit';
-
-        $score = Score::firstOrNew(['doctor_id' => $doctorId]);
-        $score->score += $incrementAmount;
-        $score->threshold += $incrementAmount;
-        $newThreshold = $score->threshold;
-
-        // Send notification if the new score exceeds 50 or its multiples
-        if ($newThreshold >= 50) {
-            // Load user object
-            $user = Auth::user();
-            // Send notification
-            $user->notify(new ReachingSpecificPoints($score));
-            $score->threshold = 0;
-        }
-
-        $score->save();
-
-        // Log score history
-        ScoreHistory::create([
-            'doctor_id' => $doctorId,
-            'score' => $incrementAmount,
-            'action' => $action,
-            'timestamp' => now(),
-        ]);
-
-        $response = [
-            'value' => true,
-            'message' => 'Final Submit Updated Successfully',
-        ];
-
-        return response()->json($response, 201);
-    }
-
-    /**
-     * Show questions and answers for a specific section and patient.
-     */
-
-    public function showQuestionsAnswers($section_id, $patient_id)
-    {
-        try {
-            // Check if the section exists
-            $sectionExists = Questions::where('section_id', $section_id)->exists();
-            if (!$sectionExists) {
-                return response()->json([
-                    'value' => false,
-                    'message' => "Section not found",
-                ], 404);
-            }
-
-            $patient = Patients::find($patient_id); // Retrieve the patient from the database
-            if (!$patient) {
-                return response()->json([
-                    'value' => false,
-                    'message' => "Patient not found",
-                ], 404);
-            }
-
-            $data = [];
-
-            // Fetch questions dynamically based on section_id
-            $questions = Questions::where('section_id', $section_id)
-                ->orderBy('id')
-                ->get();
-
-            // Fetch all answers for the patient in one query
-            $answers = Answers::where('patient_id', $patient_id)
-                ->get();
-
-            foreach ($questions as $question) {
-                // Skip questions with certain IDs
-                if ($question->skip) {
-                    Log::info("Question with ID {$question->id} skipped as per skip flag.");
-                    continue;
-                }
-
-                $questionData = [
-                    'id' => $question->id,
-                    'question' => $question->question,
-                    'values' => $question->values,
-                    'type' => $question->type,
-                    'keyboard_type' => $question->keyboard_type,
-                    'mandatory' => $question->mandatory,
-                    'updated_at' => $question->updated_at,
-                ];
-
-                // Find the answer for this question from the fetched answers
-                $answer = $answers->where('question_id', $question->id)->first();
-
-                // Get the IDs of questions of type 'multiple'
-                $multipleQuestionIds = $questions->filter(function ($question) {
-                    return $question->type === 'multiple';
-                })->pluck('id')->toArray();
-
-                $multipleQuestionAnswers = Answers::whereIn('question_id', $multipleQuestionIds)
-                    ->where('patient_id', $patient_id)
-                    ->get();
-
-                if ($question->type === 'multiple') {
-                    // Initialize the answer array
-                    $questionData['answer'] = [
-                        'answers' => [], // Initialize answers as an empty array
-                        'other_field' => "" // Set other_field to null by default
-                    ];
-                    // Find answers for this question from the fetched answers
-                    $questionAnswers = $multipleQuestionAnswers->where('question_id', $question->id);
-
-                    // Populate the answers array
-                    foreach ($questionAnswers as $answer) {
-                        if ($answer->type !== 'other') {
-                            $questionData['answer']['answers'] = $answer->answer;
-                        }
-                        if ($answer->type === 'other') {
-                            $questionData['answer']['other_field'] = $answer->answer;
-                        }
-                    }
-                } else {
-                    // For other types of questions, return the answer directly
-                    $questionData['answer'] = $answer ? $answer->answer : "";
-                }
-
-                $data[] = $questionData;
-            }
-
-            $response = [
-                'value' => true,
-                'data' => $data,
-            ];
-
-            Log::info("Questions and answers retrieved successfully for section ID {$section_id} and patient ID {$patient_id}.");
-
-            return response()->json($response, 200);
-        } catch (\Exception $e) {
-            Log::error("Error while fetching questions and answers: " . $e->getMessage());
-            return response()->json([
-                'value' => false,
-                'message' => 'Error: ' . $e->getMessage(),
-            ], 500);
-        }
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show($patient_id)
-    {
-        $submit_status = PatientStatus::where('patient_id', $patient_id)
-            ->where('key', 'submit_status')
-            ->value('status');
-
-
-        $sections = PatientStatus::select('key', 'status', 'updated_at')
-            ->where('patient_id', $patient_id)
-            ->where('key', 'LIKE', 'section_%')
-            ->get();
-
-        $patient_name = Answers::where('patient_id', $patient_id)
-            ->where('question_id', '1')
-            ->value('answer');
-
-        $doctor_Id = Patients::where('id', $patient_id)->value('doctor_id');
-
-        if (!$patient_name) {
-            return response()->json([
-                'value' => false,
-                'message' => 'Patient not found for the given patient ID.',
-            ], 404);
-        }
-
-        if (!$sections) {
-            return response()->json([
-                'value' => false,
-                'message' => 'Sections not found for the given patient ID.',
-            ], 404);
-        }
-
-        $sectionInfos = SectionsInfo::all();
-
-        $data = [];
-        foreach ($sectionInfos as $sectionInfo) {
-            if ($sectionInfo->id === 8) {
-                continue;
-            }
-            $section_id = $sectionInfo->id;
-            $section_name = $sectionInfo->section_name;
-
-            $section_data = $sections->firstWhere('key', 'section_' . $section_id);
-
-            if (!$section_data) {
-                $updated_at_value = null;
-                $section_status = false;
-            } else {
-                $updated_at_value = $section_data->updated_at;
-                $section_status = $section_data->status;
-            }
-
-
-            $section = [
-                'section_id' => $section_id,
-                'section_status' => $section_status,
-                'updated_at' => $updated_at_value,
-                'section_name' => $section_name,
-            ];
-
-            $data[] = $section;
-        }
-
-        if ($sections) {
-            return response()->json([
-                'value' => true,
-                'submit_status' => $submit_status,
-                'patient_name' => $patient_name,
-                'doctor_Id' => $doctor_Id,
-                'data' => $data,
-            ]);
-        } else {
-            return response()->json([
-                'value' => false,
-                'message' => 'Sections not found for the given patient ID.',
-            ], 404);
-        }
-    }
-
-    /**
      * Remove the specified resource from storage.
      */
-    public function destroy($id)
+    public function destroyPatient($id)
     {
         $user = auth()->user();
         // Check if the user has permission to edit articles
@@ -1025,149 +721,5 @@ class PatientsController extends Controller
             return response()->json($response, 500);
         }
     }
-
-    public function generatePatientPDF($patient_id)
-    {
-        try {
-            // Retrieve the patient from the database with related data
-            $patient = Patients::with(['doctor', 'status', 'answers'])->findOrFail($patient_id);
-
-            $data = [];
-
-            // Fetch all questions
-            $questions = Questions::with('section')->get();
-
-            // Iterate over each question
-            foreach ($questions as $question) {
-                // Skip questions with certain IDs
-                if ($question->skip) {
-                    Log::info("Question with ID {$question->id} skipped as per skip flag.");
-                    continue;
-                }
-
-                $questionData = [
-                    'id' => $question->id,
-                    'section_id' => $question->section->id ?? "test",
-                    'section_name' => $question->section->section_name ?? "Not found",
-                    'question' => $question->question,
-                    'values' => $question->values,
-                    'type' => $question->type,
-                    'keyboard_type' => $question->keyboard_type,
-                    'mandatory' => $question->mandatory,
-                    'updated_at' => $question->updated_at,
-                ];
-
-                // Find the answer for this question
-                $answer = $patient->answers->where('question_id', $question->id)->first();
-
-                if ($question->type === 'multiple') {
-                    // Initialize the answer array
-                    $questionData['answer'] = [
-                        'answers' => [], // Initialize answers as an empty array
-                        'other_field' => null // Set other_field to null by default
-                    ];
-
-                    // Find answers for this question
-                    $questionAnswers = $patient->answers->where('question_id', $question->id);
-
-                    // Populate the answers array
-                    foreach ($questionAnswers as $answer) {
-                        if ($answer->type !== 'other') {
-                            $questionData['answer']['answers'][] = $answer->answer;
-                        }
-                        if ($answer->type === 'other') {
-                            $questionData['answer']['other_field'] = $answer->answer;
-                        }
-                    }
-                } else {
-                    // For other types of questions, return the answer directly
-                    $questionData['answer'] = $answer ? $answer->answer : null;
-                }
-
-                $data[] = $questionData;
-            }
-
-            // Pass the data to the blade view
-            $pdfData = [
-                'patient' => $patient,
-                'questionData' => $data
-                // Add more data here if needed
-            ];
-
-            // Generate the PDF using the blade view and data
-            $pdf = PDF::loadView('patient_pdf', $pdfData);
-
-            // Ensure the 'pdfs' directory exists in the public disk
-            Storage::disk('public')->makeDirectory('pdfs');
-
-            // Generate a unique filename for the PDF
-            $pdfFileName = 'filename2.pdf';
-
-            // Save the PDF file to the public disk
-            Storage::disk('public')->put('pdfs/' . $pdfFileName, $pdf->output());
-
-            // Generate the URL for downloading the PDF file
-            $pdfUrl = config('app.url') . '/' . 'storage/app/public/pdfs/' . $pdfFileName;
-
-            // Return the URL to download the PDF file along with patient data
-            return response()->json(['pdf_url' => $pdfUrl, 'patient' => $data]);
-        } catch (\Exception $e) {
-            // Log and return error if an exception occurs
-            Log::error("Error while generating PDF: " . $e->getMessage());
-            return response()->json([
-                'value' => false,
-                'message' => 'Error: ' . $e->getMessage(),
-            ], 500);
-        }
-    }
-
-    public function generatePatientPDFold($patient_id)
-    {
-        // Retrieve the patient from the database
-        //$patient = Patients::findOrFail($patient_id);
-
-        $patient = Patients::select('id', 'doctor_id', 'updated_at')
-            ->where('hidden', false)
-            ->where('id', 132)
-            ->with(['doctor' => function ($query) {
-                $query->select('id', 'name', 'lname', 'image');
-            }])
-            ->with(['status' => function ($query) {
-                $query->select('id', 'patient_id', 'key', 'status');
-            }])
-            ->with(['answers' => function ($query) {
-                $query->select('id', 'patient_id', 'answer', 'question_id');
-            }])
-            ->latest('updated_at')
-            ->get();
-
-        // Pass the data to the blade view
-        $data = [
-            'patient' => $patient,
-            // Add more data here if needed
-        ];
-
-        // Generate the PDF using the blade view and data
-        $pdf = PDF::loadView('patient_pdf', $data);
-
-        //$pdf = PDF::loadHTML('<h1>Hello, this is a New PDF!</h1>');
-
-        // Ensure the 'pdfs' directory exists in the public disk
-        Storage::disk('public')->makeDirectory('pdfs');
-
-        // Generate a unique filename for the PDF
-        $pdfFileName = 'filename2.pdf';
-
-        // Save the PDF file to the public disk
-        Storage::disk('public')->put('pdfs/' . $pdfFileName, $pdf->output());
-
-        // Generate the URL for downloading the PDF file
-        // $pdfUrl = Storage::disk('public')->url('pdfs/' . $pdfFileName);
-        $pdfUrl = config('app.url') . '/' . 'storage/app/public/pdfs/' . $pdfFileName;
-
-        // Return the URL to download the PDF file
-        return response()->json(['pdf_url' => $pdfUrl]);
-    }
-
 
 }

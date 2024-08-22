bddb401bfb4a2dfe7ed7b59b463c8b0a88199074
Mohamed Ibrahim <32494528+mohamedco96@users.noreply.github.com>
2024-08-22 15:33:58 +0300

    Update Achievement.php

--- app/Models/Achievement.php
+++ app/Models/Achievement.php
@@ -11,6 +11,34 @@ class Achievement extends Model
 
     protected $fillable = ['name', 'description', 'score', 'image'];
 
+    /**
+     * Get the Achievement's image URL with prefix.
+     *
+     * @param  string|null  $value
+     * @return string|null
+     */
+    public function getImageAttribute($value): ?string
+    {
+        return $this->getPrefixedUrl($value);
+    }
+
+    /**
+     * Get the URL with prefix.
+     *
+     * @param  string|null  $value
+     * @return string|null
+     */
+    private function getPrefixedUrl($value): ?string
+    {
+        if (!$value) {
+            return null;
+        }
+
+        // Add your prefix here
+        $prefix = config('app.url') . '/' . 'storage/';
+        return $prefix . $value;
+    }
+
     public function users()
     {
         return $this->belongsToMany(User::class, 'user_achievements')->withPivot('achieved')->withTimestamps();

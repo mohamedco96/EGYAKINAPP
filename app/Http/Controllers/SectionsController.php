9c0aae5131d48ca54a7795a18fe5dfefca70bea6
Mohamed Ibrahim <32494528+mohamedco96@users.noreply.github.com>
2024-11-11 23:41:43 +0200

    Update SectionsController.php

--- app/Http/Controllers/SectionsController.php
+++ app/Http/Controllers/SectionsController.php
@@ -561,7 +561,7 @@ class SectionsController extends Controller
                 'value' => true,
                 'submit_status' => $submit_status,
                 'patient_name' => $patient_name,
-                'doctor_Id' => $doctor_Id,
+                'doctor_Id' => (string) $doctor_Id,
                 'gfr' => $GFR,
                 'data' => $data,
             ]);

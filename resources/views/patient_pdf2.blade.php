52e0b91510a6ad6473e32d2b7a0ce3384e9ce533
Mohamed Ibrahim <32494528+mohamedco96@users.noreply.github.com>
2025-01-11 17:02:50 +0200

    Update patient_pdf2.blade.php

--- resources/views/patient_pdf2.blade.php
+++ resources/views/patient_pdf2.blade.php
@@ -240,7 +240,7 @@ $answers = collect($patient->answers)->keyBy('question_id');
                         <td class="Patient-Information-background">Age</td>
                         <td>{{ $patientAge }}</td>
                         <td class="Patient-Information-background">Gender</td>
-                        <td>{{$answers[8]['answer'] ?? null}}</td>
+                        <td>{{$answers[8]['answer'] ?? 'Not Provided' }}</td> <!-- Gender -->
                     </tr>
                     <tr>
                         <td class="Patient-Information-background">Occupation</td>

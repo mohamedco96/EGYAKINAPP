b92bad985c35487051975e2deb8afdbebb5dd2ee
Mohamed Ibrahim <32494528+mohamedco96@users.noreply.github.com>
2025-01-12 19:47:55 +0200

    Update patient_pdf2.blade.php

--- resources/views/patient_pdf2.blade.php
+++ resources/views/patient_pdf2.blade.php
@@ -210,7 +210,7 @@ $answers = collect($patient->answers)->keyBy('question_id');
                         <td class="Patient-Information-background">Age</td>
                         <td></td>
                         <td class="Patient-Information-background">Gender</td>
-                        <td>{{ collect($questionData)->where('id', 8)->first()['answer']['answers'] ?? 'Not Provided' }}</td>
+                        <!-- <td>{{ collect($questionData)->where('id', 8)->first()['answer']['answers'] ?? 'Not Provided' }}</td> -->
                     </tr>
                     <tr>
                         <td class="Patient-Information-background">Occupation</td>

d1e271812d12945a8dc8e90a10c90bcaed143de3
Mohamed Ibrahim <32494528+mohamedco96@users.noreply.github.com>
2025-03-25 08:13:11 +0200

    Update FeedPostController.php

--- app/Http/Controllers/FeedPostController.php
+++ app/Http/Controllers/FeedPostController.php
@@ -525,8 +525,9 @@ class FeedPostController extends Controller
             $this->handleGroupValidation($validatedData);
 
             // Handle media upload
-            $mediaPaths = $this->handleMediaUpload($request, $validatedData['media_type'] ?? null);
-
+            //$mediaPaths = $this->handleMediaUpload($request, $validatedData['media_type'] ?? null);
+            $mediaPaths = $this->uploadMultipleImages($request);
+            Log::info("Media Paths: ", $mediaPaths);
             // Create Feed Post
             $post = $this->createFeedPost($validatedData, $mediaPaths);
 
@@ -721,6 +722,40 @@ class FeedPostController extends Controller
         }
 
         return null;
+    }
+
+    private function uploadMultipleImages(Request $request)
+    {
+        // Validate the request
+        $request->validate([
+            'media_path.*' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+        ]);
+
+        $uploadedImages = [];
+
+        if ($request->hasFile('media_path')) {
+            foreach ($request->file('media_path') as $media) {
+                // Get the authenticated user's name
+                $name = auth()->user()->name;
+
+                // Generate a unique timestamp
+                $timestamp = time();
+
+                // Create a unique file name using the user's name and timestamp
+                $fileName = "{$name}_media_{$timestamp}." . $media->getClientOriginalExtension();
+
+                // Store the media in the specified path (image or video directory)
+                $storedPath = $media->storeAs('media_images', $fileName, 'public');
+
+                // Construct the full URL for the uploaded media
+                $mediaUrl = config('app.url') . '/storage/' . $storedPath;
+                
+                // Get the public URL of the uploaded file
+                $uploadedImages[] = $mediaUrl;
+            }
+        }
+
+        return $uploadedImages;
     }
 
     private function createFeedPost(array $validatedData, $mediaPaths)

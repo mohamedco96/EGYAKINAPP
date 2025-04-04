0a0bdc6e9b3cd7bde12d3cb821d5abd84e95a32f
Mohamed Ibrahim <32494528+mohamedco96@users.noreply.github.com>
2025-04-04 21:24:50 +0200

    Update AuthController.php

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -189,7 +189,7 @@ class AuthController extends Controller
             } catch (ValidationException $e) {
                 return response()->json([
                     'value' => false,
-                    'message' => 'Validation failed',
+                    'message' => array_values($e->errors())[0][0],
                     'errors' => $e->errors()
                 ], 422);
             }
@@ -311,7 +311,7 @@ class AuthController extends Controller
             } catch (ValidationException $e) {
                 return response()->json([
                     'value' => false,
-                    'message' => 'Validation failed',
+                    'message' => array_values($e->errors())[0][0],
                     'errors' => $e->errors()
                 ], 422);
             }
@@ -548,7 +548,7 @@ class AuthController extends Controller
             } catch (ValidationException $e) {
                 return response()->json([
                     'value' => false,
-                    'message' => 'Validation failed',
+                    'message' => array_values($e->errors())[0][0],
                     'errors' => $e->errors()
                 ], 422);
             }
